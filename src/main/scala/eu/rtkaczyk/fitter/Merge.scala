bde350fad76c95adc9777bc4ebeacaa9a586f33e
Radek Tkaczyk <rtkaczyk@users.noreply.github.com>
2017-05-25 03:26:42 +0200

    proper merging wip

--- src/main/scala/eu/rtkaczyk/fitter/Merge.scala
+++ src/main/scala/eu/rtkaczyk/fitter/Merge.scala
@@ -12,35 +12,80 @@ object Merge extends Mode {
 
     val encoder = new FileEncoder(new java.io.File(output), Fit.ProtocolVersion.V1_0)
 
+    var lastActivity: ActivityMesg = null
+
     for ((input, i) <- inputs.zipWithIndex) {
-      val listener = new Listener(encoder, i == 0, i == inputs.size - 1)
+      val listener = new Listener(encoder, lastActivity, i == 0, i == inputs.size - 1)
       val decoder = new Decode
       val in = new FileInputStream(input)
 
       decoder.read(in, listener, listener)
       in.close()
+
+      lastActivity = listener.lastActivity
     }
 
     encoder.close()
   }
 
-  class Listener(encoder: FileEncoder, first: Boolean, last: Boolean) extends MesgListener with MesgDefinitionListener {
-    def onMesg(mesg: Mesg): Unit = mesg match {
-      case _: FileIdMesg =>
+  class Listener(encoder: FileEncoder, prevActivity: ActivityMesg, first: Boolean, last: Boolean)
+    extends MesgListener with MesgDefinitionListener {
+
+    var lastActivity: ActivityMesg = prevActivity
+
+    def onMesg(mesg: Mesg): Unit = mesg.getNum match {
+      case MesgNum.FILE_ID | MesgNum.FILE_CREATOR =>
         if (first)
           encoder.write(mesg)
 
+      case MesgNum.ACTIVITY =>
+        val activity = new ActivityMesg(mesg)
+        lastActivity = joinActivities(lastActivity, activity)
+        if (last)
+          encoder.write(lastActivity)
+
+        println(s"Last activity timestamp: ${lastActivity.getTimestamp.getTimestamp} " +
+          s"(${Option(lastActivity).map(_.getTimestamp.getTimestamp.toLong).getOrElse(0L)})")
+
       case _ =>
+        Option(mesg.getField("timestamp")).map { f =>
+          val t = f.getValue.asInstanceOf[Long]
+          f.setValue(t + Option(lastActivity).map(_.getTimestamp.getTimestamp.toLong).getOrElse(0L))
+          f
+        }.foreach(mesg.setField)
         encoder.write(mesg)
     }
 
     def onMesgDefinition(mesg: MesgDefinition): Unit = mesg.getNum match {
-      case MesgNum.FILE_ID  =>
+      case MesgNum.FILE_ID | MesgNum.FILE_CREATOR =>
         if (first)
+          encoder.write(mesg)
+
+      case MesgNum.ACTIVITY =>
+        if (last)
           encoder.write(mesg)
 
       case _ =>
         encoder.write(mesg)
     }
+
+    def joinActivities(prev: ActivityMesg, curr: ActivityMesg): ActivityMesg = {
+      if (prev == null)
+        curr
+      else {
+        val joined = new ActivityMesg()
+        joined.setTimestamp {
+          val d = prev.getTimestamp
+          d.add(curr.getTimestamp)
+          d
+        }
+        joined.setTotalTimerTime(prev.getTotalTimerTime + curr.getTotalTimerTime)
+        joined.setNumSessions(prev.getNumSessions + curr.getNumSessions)
+
+        joined
+      }
+    }
+
   }
+
 }
